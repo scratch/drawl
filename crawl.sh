dacfb665c51f3a4531b62b1b6144514c5170d690
Krishnan. N <scratch@srijan.in>
2013-06-26 20:17:26 +0530

    -- nk added comments

--- crawl.sh
+++ crawl.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+# crawl.sh -- Find URLs which are Drupal sites.
+
+# Invariant: The first field of CSV should be the
+#            URL to crawl.
+#
 for i in `cat $1 | cut -d, -f1`
 do
     # test
     #num=`cat xx |grep -i drupal | wc -l`
+    #num=`curl -sIL drupal.org |grep -i drupal | wc -l`
 
     printf "Checking site: %s --  " $i
-    #num=`curl -sIL drupal.org |grep -i drupal | wc -l`
     curl -sL $i -D /tmp/header.log > /tmp/full.html
 
     hdrhasdrupal=`grep -i drupal /tmp/header.log | wc -l`
