18a73457b05a85d11a3421f6f94e23cbe638bc8e
Krishnan. N <scratch@srijan.in>
2013-08-19 12:00:54 +0530

    Hosting provider info implemented

--- crawl.sh
+++ crawl.sh
@@ -3,34 +3,40 @@
 #
 # Usage: ./crawl.sh <hosting csv file>
 #
-# Invariant: The first field of CSV should be the
-#            URL to crawl.
+# Invariant: 
+#   1. The first field of hosting CSV should be the  URL to crawl.
+#   2. Expects a file called webhosting.csv that has list of 
+#      hosting providers, one per line.
+#
 #
 
-# HostingProviderInfo: display details about hosting provider.
+
+
+# HostingProviderInfo: use dig cmd (much faster) to see if service 
+#     provider matches, else take the slower traceroute option.
 #
 #
-function HostingProviderInfo()
-{
-    traceroute $1 > /tmp/xx
-    grep -iw -f  webhosting.csv  /tmp/xx
-    if [ $? == 0 ]
+function HostingProviderInfo() {
+    dig $1 > /tmp/xx
+    grep -iw --only-matching -m 1 -f  webhosting.csv  /tmp/xx
+    if [ $? != 0 ]
     then
-	printf "Hosting site found"
-    else
-	printf "Hosting site NOT found"
+	traceroute $1 > /tmp/xx
+	grep -iw --only-matching -m 1 -f  webhosting.csv  /tmp/xx
     fi
+
+    return $?;
 }
 
 
-
+# main()
 for i in `cat $1 | cut -d, -f1`  # parse CSV file and get the first field
 do
     # test
     #num=`cat xx |grep -i drupal | wc -l`
     #num=`curl -sIL drupal.org |grep -i drupal | wc -l`
 
-    printf "Checking site: %s " $i
+    printf "Checking site: %s -- " $i
     curl -sL $i -D /tmp/header.log > /tmp/full.html
 
     # TODO: 
@@ -38,14 +44,15 @@ do
     hdrhasdrupal=`grep -i drupal /tmp/header.log | wc -l`
     if (( $hdrhasdrupal > 0 ))
     then
-	echo "Drupal site"
+	printf "Drupal site\n"
 	HostingProviderInfo $i
     else
 	printf "possily other site ..."
 	numdrupaldirs=`grep "sites.default" /tmp/full.html | wc -l`
 	if (( $numdrupaldirs > 0 ))
 	then
-	   printf "Contains Drupal Dir structures; Drupal site\n"
+	    printf "Contains Drupal Dir structures; Drupal site\n"
+	    HostingProviderInfo $i
 	else
 	    printf " other site\n"
 	fi
